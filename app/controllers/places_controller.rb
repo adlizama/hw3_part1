eb1b7f6e925d98e4f931610bafd5c64c91fe5320
Anthony Lizama <anthony.lizama@kellogg.northwestern.edu>
2024-02-19 22:53:17 +0000

    4:53 pm

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,12 +1,31 @@
 class PlacesController < ApplicationController
 
 def index
-  render :template => "places/index"
+  #render :template => "places/index"
+  @place = Place.all
+  
 end
+
+def new
+  @place = Place.new
+
+end
+
 
 def create
   #start with new place
-  @place = Places.new
+  @place = Place.new
+
+  #assign user-entered form data to Place's columns
+  @place["name"] = params["name"]
+
+  #save Place row
+
+  @place.save
+
+  #redirect user
+  
+  redirect_to "/places"
 
 
 end
