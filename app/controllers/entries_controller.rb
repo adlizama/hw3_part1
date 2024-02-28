a0d3c6596422adcf7acdb068330ad20536e6114a
Anthony Lizama <anthony.lizama@kellogg.northwestern.edu>
2024-02-28 04:19:46 +0000

    making the entries 10:19

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -5,4 +5,25 @@ class EntriesController < ApplicationController
     render :inline => "<h1>Welcome Entry Enthusiast</h1>"
   end
 
+  def create
+    #start with new entry
+    @entry = Entry.new
+
+    #assign user-entered form data to Entry's columns
+    @entry["title"] = params["title"]
+    @entry["description"] = params["description"]
+    @entry["posted_on"] = params["posted_on"]
+    
+    #save Entry ow
+    @entry.save
+
+    #redirect user  
+    redirect to "/places"
+  end
+
+  def show
+
+  end
+  
+
 end
